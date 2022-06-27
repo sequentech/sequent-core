e411752f845609cb25cb8d5f17a1bd01f0bd4ab9
Findeton <felix@sequentech.io>
2022-06-27 10:30:01 +0100

    update strand dep

--- src/encrypt.rs
+++ src/encrypt.rs
@@ -2,12 +2,12 @@ use crate::ballot::*;
 use ed25519_dalek::{Digest, Sha512};
 use num_bigint::BigUint;
 use num_traits::Num;
-use strand::backend::numb::{BigintCtx, P2048};
+use strand::backend::numb::{BigUintE, BigUintX, BigintCtx, P2048};
 use strand::context::Ctx;
 use strand::elgamal::*;
 
 quick_error! {
-    #[derive(Debug)]
+    #[derive(Debug, PartialEq)]
     pub enum BallotError {
         ParseBigUint(uint_str: String, message: String) {}
         CryptographicCheck(message: String) {}
@@ -24,36 +24,40 @@ fn recreate_encrypt_answer(
     let ctx = BigintCtx::<P2048>::new();
 
     // create public key
-    let pk_bigint = BigUint::from_str_radix(&public_key.public_key, 10).map_err(|_| {
-        BallotError::ParseBigUint(
-            public_key.public_key.clone(),
-            String::from("Error parsing public key"),
-        )
-    })?;
+    let pk_bigint = BigUintE(BigUint::from_str_radix(&public_key.public_key, 10).map_err(
+        |_| {
+            BallotError::ParseBigUint(
+                public_key.public_key.clone(),
+                String::from("Error parsing public key"),
+            )
+        },
+    )?);
     let pk = PublicKey::from_element(&pk_bigint, &ctx);
 
     // parse plaintext
-    let plaintext = BigUint::from_str_radix(&choice.plaintext, 10).map_err(|_| {
+    let plaintext = BigUintE(BigUint::from_str_radix(&choice.plaintext, 10).map_err(|_| {
         BallotError::ParseBigUint(
             choice.plaintext.clone(),
             String::from("Error parsing plaintext"),
         )
-    })?;
+    })?);
 
     // parse randomness
-    let randomness = BigUint::from_str_radix(&choice.randomness, 10).map_err(|_| {
-        BallotError::ParseBigUint(
-            choice.randomness.clone(),
-            String::from("Error parsing randomness"),
-        )
-    })?;
+    let randomness = BigUintX(
+        BigUint::from_str_radix(&choice.randomness, 10).map_err(|_| {
+            BallotError::ParseBigUint(
+                choice.randomness.clone(),
+                String::from("Error parsing randomness"),
+            )
+        })?,
+    );
 
     // sanity checks
-    if !ctx.is_valid_element(&plaintext) {
+    /*if !ctx.is_valid_element(&plaintext) {
         return Err(BallotError::CryptographicCheck(String::from(
             "Invalid plaintext",
         )));
-    }
+    }*/
     if KeyType::P2048 != public_key.key_type {
         return Err(BallotError::ConsistencyCheck(String::from(
             "Invalid key type",
@@ -61,12 +65,12 @@ fn recreate_encrypt_answer(
     }
 
     // encrypt / create cyphertext
-    let cyphertext = pk.encrypt_ext(&plaintext, &randomness);
+    let cyphertext = pk.encrypt_with_randomness(&plaintext, &randomness);
 
     // convert to output format
     Ok(CyphertextChoice {
-        gr: cyphertext.gr().to_str_radix(10),
-        mhr: cyphertext.mhr().to_str_radix(10),
+        gr: cyphertext.gr().0.to_str_radix(10),
+        mhr: cyphertext.mhr().0.to_str_radix(10),
     })
 }
 
@@ -108,16 +112,20 @@ pub fn hash_to(ballot: &Ballot) -> Result<String, BallotError> {
 }
 
 #[cfg(test)]
-mod tests {
+mod testes {
     use crate::ballot::*;
     use crate::encrypt::*;
     use std::fs;
 
-    #[test]
-    fn parse_ballot() {
+    fn read_ballot_fixture() -> Ballot {
         let contents = fs::read_to_string("fixtures/ballot.json")
             .expect("Something went wrong reading the file");
-        let ballot: Ballot = serde_json::from_str(&contents).unwrap();
+        serde_json::from_str(&contents).unwrap()
+    }
+
+    #[test]
+    fn test_parse_ballot() {
+        let ballot = read_ballot_fixture();
         let sha512_ballot = hash_to(&ballot).unwrap();
         assert_eq!(&sha512_ballot, &ballot.ballot_hash);
         assert_eq!(
@@ -126,5 +134,22 @@ mod tests {
         );
         let recreated_cyphertext = recreate_encrypt_cyphertext(&ballot).unwrap();
         assert_eq!(recreated_cyphertext, ballot.cyphertext);
+    }
+
+    #[test]
+    fn test_recreate_encrypt_answer() {
+        let ballot = read_ballot_fixture();
+        let pk = Pk {
+            key_type: KeyType::P2048,
+            public_key: "invalid_key".to_string(),
+        };
+        let call_result = recreate_encrypt_answer(&pk, &ballot.replication.choices[0]);
+        assert_eq!(
+            call_result,
+            Err(BallotError::ParseBigUint(
+                pk.public_key.clone(),
+                String::from("Error parsing public key"),
+            ))
+        );
     }
 }
